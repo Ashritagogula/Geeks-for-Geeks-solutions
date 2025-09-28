fea3ab2ec86563fbad86fe0445033dedb952bb14
Ashrita Gogula <ashritagogula12@gmail.com>
2025-09-28 22:27:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Arithmetic Operators/arithmetic-operators.py
@@ -0,0 +1,15 @@
+x = int(input())
+y = int(input())
+
+# Perform addition x+y
+p = x + y
+# Perform subtraction x-y
+q = x - y
+# Perform multiplication x*y
+r = x * y
+# Perform integer division x//y
+s = x // y
+# Perform modulo operation x%y
+t = x % y
+
+print(p, q, r, s, t)
