e2c498667f0fd871a75c4f4d3437393f21087998
Ashrita Gogula <ashritagogula12@gmail.com>
2025-10-11 10:57:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/The Else Statement/the-else-statement.py
@@ -0,0 +1,11 @@
+#User function Template for python3
+
+def utility(number):
+    #Write your if-else statement below
+    if number > 100:
+        print("Big")
+    elif number <= 100:
+        print("Number")
+    
+    
+    #Write your if-else statement above
